f7653a6341c1d473398779fc87de9d962bbe6818
Polly <23667108+PollyannaRibeiro@users.noreply.github.com>
2019-07-28 13:08:14 +0100

    added all modal content and pictures

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
-function openSlideMenu(){
-  // document.getElementById("side-menu").style.marginLeft = '0px';
-  $("#side-menu").css({"marginLeft":"0", "transition":"0.5s"});
+// Responsive Menu
 
+function openSlideMenu(){
+  $("#side-menu").css({"marginLeft":"0", "transition":"0.5s"});
 }
 
 function closeSideMenu(){
   $("#side-menu").css({"marginLeft":"-250px", "transition":"0.5s"})
 }
+
+// Fade in
+
+$(function(){  // $(document).ready shorthand
+  $('.projects').fadeIn('slow');
+});
+
+$(document).ready(function() {
+    
+    /* Every time the window is scrolled ... */
+    $(window).scroll( function(){
+    
+        /* Check the location of each desired element */
+        $('.hideme').each( function(i){
+            
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+                
+                $(this).animate({'opacity':'1'},1500);
+                    
+            }
+            
+        }); 
+    
+    });
+    
+});
